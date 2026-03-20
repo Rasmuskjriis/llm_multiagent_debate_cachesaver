805943679607e34e02396cc9e792aa060c75675c
rasmus <rasmusoleander@live.dk>
2026-03-20 15:41:27 +0100

    Commented away semaphores

--- math/gen_math.py
+++ math/gen_math.py
@@ -26,19 +26,19 @@ def parse_bullets(sentence):
 
     return bullets
 
-semaphore = asyncio.Semaphore(1)
+#semaphore = asyncio.Semaphore(1)
 
 async def generate_answer(client, answer_context):
-    async with semaphore:
-        try:
-            completion = await client.chat.completions.create(
-                    messages=answer_context,
-                    model="qwen3:0.6b")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            print("retrying due to an error......")
-            await asyncio.sleep(5)
-            return await generate_answer(client, answer_context)
+    #async with semaphore:
+    try:
+        completion = await client.chat.completions.create(
+                messages=answer_context,
+                model="qwen3:0.6b")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("retrying due to an error......")
+        await asyncio.sleep(5)
+        return await generate_answer(client, answer_context)
 
     return completion
 
