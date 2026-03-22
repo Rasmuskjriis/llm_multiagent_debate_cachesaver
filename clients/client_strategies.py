856214854f54d41b83d0f48c0d5745894c4d604d
rasm2 <rasm202m@gmail.com>
2026-03-22 20:58:29 +0100

    Expanded client_stategies to include Ollama client without CacheSaver.

--- clients/client_strategies.py
+++ clients/client_strategies.py
@@ -1,19 +1,34 @@
 from abc import ABC, abstractmethod
 
-from cachesaver.models.openai import AsyncOpenAI
+from cachesaver.models.openai import AsyncOpenAI as CacheSaverAsyncOpenAI
+from openai import AsyncOpenAI
 
 class ClientStrategy(ABC):
     @abstractmethod
     def create_chat_completion(self):
         pass
 
-class LocalOllamaClient(ClientStrategy):
-    def __init__(self, model, cachesaver=True):
-        self.client = AsyncOpenAI(
+class CacheSaverOllamaClient(ClientStrategy):
+    def __init__(self, model):
+        self.client = CacheSaverAsyncOpenAI(
             base_url='http://localhost:11434/v1/',
             api_key='ollama',  # required but ignored
             namespace="local_ollama_" + model,
-            cachedir="../cache" if cachesaver else None
+            cachedir="../cache"
+        )
+        self.model = model
+
+    def create_chat_completion(self, messages, n=1):
+        return self.client.chat.completions.create(
+                    messages=messages,
+                    model=self.model,
+                    n=n)
+
+class OllamaClient(ClientStrategy):
+    def __init__(self, model):
+        self.client = AsyncOpenAI(
+            base_url='http://localhost:11434/v1/',
+            api_key='ollama',  # required but ignored
         )
         self.model = model
 
