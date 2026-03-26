153b05e46bf2c8c90e78cbbdd908c22ecf32e185
rasm2 <rasm202m@gmail.com>
2026-03-26 14:47:09 +0100

    Added clients for qroq and openAI inference service.

--- clients/client_strategies.py
+++ clients/client_strategies.py
@@ -1,7 +1,9 @@
 from abc import ABC, abstractmethod
+import os
 
 from cachesaver.models.openai import AsyncOpenAI as CacheSaverAsyncOpenAI
 from openai import AsyncOpenAI
+from groq import Groq
 
 class ClientStrategy(ABC):
     @abstractmethod
@@ -10,7 +12,7 @@ class ClientStrategy(ABC):
 
 class CacheSaverOllamaClient(ClientStrategy):
     def __init__(self, model):
-        self.client = CacheSaverAsyncOpenAI(
+        self.client = CacheSaverAsyncOp enAI(
             base_url='http://localhost:11434/v1/',
             api_key='ollama',  # required but ignored
             namespace="local_ollama_" + model,
@@ -33,6 +35,51 @@ class OllamaClient(ClientStrategy):
         )
         self.model = model
 
+    def create_chat_completion(self, messages, n=1):
+        return self.client.chat.completions.create(
+                    messages=messages,
+                    model=self.model,
+                    n=n
+        )
+
+class CacheSaverOpenAIClient(ClientStrategy):
+    def __init__(self, model):
+        self.client = CacheSaverAsyncOpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY"),
+            namespace="openai_" + model,
+            cachedir="./cache"
+        )
+        self.model = model
+
+    def create_chat_completion(self, messages, n=1):
+        return self.client.chat.completions.create(
+                    messages=messages,
+                    model=self.model,
+                    n=n
+        )
+
+
+class OpenAIClient(ClientStrategy):
+    def __init__(self, model):
+        self.client = AsyncOpenAI(
+            api_key=os.environ.get("OPENAI_API_KEY")
+        )
+        self.model = model
+
+    def create_chat_completion(self, messages, n=1):
+        return self.client.chat.completions.create(
+                    messages=messages,
+                    model=self.model,
+                    n=n
+        )
+    
+class GroqClient(ClientStrategy):
+    def __init__(self, model):
+        self.client = Groq(
+            api_key=os.environ.get("GROQ_API_KEY")
+        )
+        self.model = model
+    
     def create_chat_completion(self, messages, n=1):
         return self.client.chat.completions.create(
                     messages=messages,
