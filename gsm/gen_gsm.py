1099dd8284c045d71fb7ee3eac853cb5062d4ea5
rasmus <rasmusoleander@live.dk>
2026-03-31 00:03:43 +0200

    Implemented minimal working version of gen_gsm.py although without any kind of logging yet

--- gsm/gen_gsm.py
+++ gsm/gen_gsm.py
@@ -2,6 +2,23 @@ import openai
 import json
 import numpy as np
 import random
+import asyncio
+import argparse
+
+import clients.client_strategies as clients
+
+async def generate_answer(client, answer_context):
+    try:
+        completion = await client.create_chat_completion(
+            messages = answer_context
+            )
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("retrying due to an error......")
+        await asyncio.sleep(5)
+        return await generate_answer(client, answer_context)
+
+    return completion
 
 def construct_message(agents, question, idx):
     if len(agents) == 0:
@@ -20,7 +37,7 @@ def construct_message(agents, question, idx):
 
 
 def construct_assistant_message(completion):
-    content = completion["choices"][0]["message"]["content"]
+    content = completion.choices[0].message.content
     return {"role": "assistant", "content": content}
 
 
@@ -28,23 +45,28 @@ def read_jsonl(path: str):
     with open(path) as fh:
         return [json.loads(line) for line in fh.readlines() if line]
 
-if __name__ == "__main__":
-    agents = 3
-    rounds = 2
+
+async def main(agents, rounds, questions, model, use_cachesaver):
+    if use_cachesaver:
+        client = clients.CacheSaverAsyncGroq(model=model)
+    else:
+        client = clients.GroqClient(model=model)
+
     random.seed(0)
 
     generated_description = {}
 
-    questions = read_jsonl("gsm/data/test.jsonl")
-    random.shuffle(questions)
+    test_questions = read_jsonl("gsm/data/test.jsonl")
+    random.shuffle(test_questions)
 
-    for data in questions[:100]:
+    for data in test_questions[:questions]:
         question = data['question']
         answer = data['answer']
 
         agent_contexts = [[{"role": "user", "content": """Can you solve the following math problem? {} Explain your reasoning. Your final answer should be a single numerical number, in the form \\boxed{{answer}}, at the end of your response. """.format(question)}] for agent in range(agents)]
 
         for round in range(rounds):
+            tasks = []
             for i, agent_context in enumerate(agent_contexts):
 
                 if round != 0:
@@ -52,19 +74,38 @@ if __name__ == "__main__":
                     message = construct_message(agent_contexts_other, question, 2*round - 1)
                     agent_context.append(message)
 
-                completion = openai.ChatCompletion.create(
-                          model="gpt-3.5-turbo-0301",
-                          messages=agent_context,
-                          n=1)
+                tasks.append(generate_answer(client, agent_context))
 
-                assistant_message = construct_assistant_message(completion)
+            completions = await asyncio.gather(*tasks)
+
+            for i, agent_context in enumerate(agent_contexts):
+                assistant_message = construct_assistant_message(completions[i])
                 agent_context.append(assistant_message)
 
         generated_description[question] = (agent_contexts, answer)
 
-    json.dump(generated_description, open("gsm_{}_{}.json".format(agents, rounds), "w"))
+    json.dump(generated_description, open("gsm/results/gsm_{}_{}.json".format(agents, rounds), "w"))
 
-    import pdb
-    pdb.set_trace()
     print(answer)
     print(agent_context)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-a", "--agents", type=int, default=2)
+    parser.add_argument("-r", "--rounds", type=int, default=3)
+    parser.add_argument("-q", "--questions", type=int, default=10)
+    parser.add_argument("-m","--model", type=str, default="qwen3:0.6b")
+    parser.add_argument("-c","--cachesaver", action="store_true", dest="use_cachesaver")
+
+    args = parser.parse_args()
+
+    asyncio.run(
+        main(
+            agents=args.agents, 
+            rounds=args.rounds,
+            questions=args.questions,
+            model=args.model,
+            use_cachesaver=args.use_cachesaver
+        )
+    )
